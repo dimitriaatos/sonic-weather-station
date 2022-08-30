d1d9ad60e9d71c41e8db9f4f1dfea186befbe229
M1Z1 <gmizithras@gmail.com>
2022-08-31 00:36:11 +0300

    soundEngine

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
+import {init, toneFunc, stopFunc} from './soundEngine'
 
 const App = () => {
 
 	return (
 		<div>
 			<h1>Sonic Weather Station</h1>
-			<button type="button">init</button>
-			<button type="button" onclick="toneFunc()">Soundon!</button>
-			<button type="button" onclick="stopFunc()">Stop!</button>
+			<button type="button" onClick={init}>init</button>
+			<button type="button" onClick={toneFunc}>Soundon!</button>
+			<button type="button" onClick={stopFunc}>Stop!</button>
 		</div>
 
 	)
