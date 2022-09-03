ed1018f2e226d3765bbaf2b2e90ba46891c99f4c
dimitriaatos <d.a.ellinas@gmail.com>
2022-09-04 01:48:45 +0300

    dots using client mouse position

--- src/Dots.jsx
+++ src/Dots.jsx
@@ -1,29 +1,28 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { dotPositions } from './dotsData'
 
 const Dots = (props) => {
 	const [coords, setCoords] = useState({ x: 0, y: 0 })
-	const [replaceCursor, setReplaceCursor] = useState(false)
+	const [dotsActive, setDotsActive] = useState(false)
+	const dotsContainer = useRef()
 
-	const handleMouseMove = event => {
-		setCoords({
-			x: event.clientX - event.currentTarget.offsetLeft,
-			y: event.clientY - event.currentTarget.offsetTop,
-		});
-		props?.onChange?.(coords)
-	}
+	useEffect(() => {
+		const handleWindowMouseMove = event => {
+			const dotsBounds = dotsContainer.current.getBoundingClientRect()
+			const newCoords = {
+				x: event.clientX - dotsBounds.left,
+				y: event.clientY - dotsBounds.top,
+			}
+			setCoords(newCoords);
+			props?.onChange?.(newCoords)
+		};
 
-	const onLeave = () => {
-		setReplaceCursor(false)
-	}
+		document.addEventListener('mousemove', handleWindowMouseMove);
 
-	const onEnter = () => {
-		setReplaceCursor(true)
-	}
-
-	// useEffect(() => {
-	// 	console.log(replaceCursor ? 'visible' : 'hidden')
-	// }, [replaceCursor])
+		return () => {
+			document.removeEventListener('mousemove', handleWindowMouseMove);
+		};
+	}, []);
 
 	const calcRadius = (x, y) => {
 		const distance = Math.sqrt(Math.pow(x - coords.x, 2) + Math.pow(y - coords.y, 2))
@@ -34,14 +33,18 @@ const Dots = (props) => {
 		<div
 			style={{
 				width: '100%',
-				// border: 'solid green 2px'
 			}}
 		>
 			<div
-				onMouseLeave={onLeave}
-				onMouseEnter={onEnter}
-				onMouseMove={handleMouseMove}
-				style={{ margin: 'auto', width: 420, cursor: replaceCursor ? 'none' : 'default' }}
+				onMouseLeave={() => setDotsActive(false)}
+				onMouseEnter={() => setDotsActive(true)}
+				ref={dotsContainer}
+				style={{
+					margin: 'auto',
+					width: 420,
+					height: 420,
+					cursor: dotsActive ? 'none' : 'default',
+				}}
 			>
 				<svg
 					width="420px"
@@ -55,26 +58,12 @@ const Dots = (props) => {
 						{
 							dotPositions.map(([x, y], index) => {
 								return (
-									// <g key={index}>
-									// 	<line x1={x} y1={y} x2={coords.x} y2={coords.y} stroke="#979797"></line>
-
-									// 	<defs>
-									// 		<linearGradient x1="40%" y1="40%" x2="100%" y2="100%" id={`linearGradient-${x}${y}`}>
-									// 			<stop stopColor="red" stopOpacity="0" offset="0.00%"></stop>
-									// 			<stop stopColor="green" offset="100%"></stop>
-									// 		</linearGradient>
-									// 	</defs>
-									// 	<g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="square">
-									// 		<line x1={x} y1={y} x2={coords.x} y2={coords.y} stroke={`url(#linearGradient-${x}${y})`}></line>
-									// 	</g>
-									// 	<circle cx={x} cy={y} r={calcRadius(x, y)}></circle>
-									// </g>
 									<circle cx={x} cy={y} r={calcRadius(x, y)} key={index}></circle>
 								)
 							})
 						}
 					</g>
-					<g stroke="#979797" fill="none" visibility={replaceCursor ? 'visible' : 'hidden'}>
+					<g stroke="#979797" fill="none">
 						<circle cx={coords.x} cy={coords.y} r="15"></circle>
 						<circle cx={coords.x} cy={coords.y} r="12"></circle>
 					</g>
