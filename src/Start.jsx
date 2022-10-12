c1763f798db39549ac8954fd9bd018449926b15d
dimitriaatos <d.a.ellinas@gmail.com>
2022-10-12 19:06:24 +0300

    enable startText without flags

--- src/Start.jsx
+++ src/Start.jsx
@@ -4,22 +4,18 @@ import './Start.css'
 const Start = (props) => {
 	return (
 		<div className="start">
-			<Flags authorizedFlags={['startText']}>
-				<div className="info">
-					<span>STAVROS GASPARATOS</span>
-					<br />
-					Sonic Weather Station, 2022 - ongoing
-				</div>
-			</Flags>
+			<div className="info">
+				<span>STAVROS GASPARATOS</span>
+				<br />
+				Sonic Weather Station, 2022 - ongoing
+			</div>
 			<h1>Sonic Weather Station</h1>
-			<Flags authorizedFlags={['startText']}>
-				<div className="main-text">
-					The Station is an online programme that generates sonic output by
-					<br />
-					receiving combined weather data from a physical weather station in
-					Greece.
-				</div>
-			</Flags>
+			<div className="main-text">
+				The Station is an online programme that generates sonic output by
+				<br />
+				receiving combined weather data from a physical weather station in
+				Greece.
+			</div>
 			<button
 				type="button"
 				className="start-button"
@@ -27,34 +23,32 @@ const Start = (props) => {
 			>
 				Enter
 			</button>
-			<Flags authorizedFlags={['startText']}>
-				<footer>
-					<div className="main-footer">
-						Commissioned by NEON, for &apos;112, Climate Tone&apos; as part of
-						the World Weather Network © the artist, Courtesy NEON
+			<footer>
+				<div className="main-footer">
+					Commissioned by NEON, for &apos;112, Climate Tone&apos; as part of the
+					World Weather Network © the artist, Courtesy NEON
+				</div>
+				<div className="links">
+					<a href="http://www.stavrosgasparatos.com">Stavros Gasparatos</a>
+					<a href="https://neon.org.gr">NEON</a>
+					<a href="https://neon.org.gr/en/exhibition/112-climate-tone-2/">
+						112, Climate Tone
+					</a>
+					<a href="https://worldweathernetwork.org/">World Weather Network</a>
+				</div>
+				<div className="credits">
+					<div>
+						Live Weather Data: Automatic Meteorological Station - Park of
+						Aristotle University of Thessaloniki; Department of Meteorology and
+						Climatology, In collaboration with Professor Prodromos Zanis
 					</div>
-					<div className="links">
-						<a href="http://www.stavrosgasparatos.com">Stavros Gasparatos</a>
-						<a href="https://neon.org.gr">NEON</a>
-						<a href="https://neon.org.gr/en/exhibition/112-climate-tone-2/">
-							112, Climate Tone
-						</a>
-						<a href="https://worldweathernetwork.org/">World Weather Network</a>
+					<div>
+						Programming - Software Development: Dimitri Aatos Ellinas
+						<span> </span>
+						Audio Implementation - Sonification: Georgios Mizithras
 					</div>
-					<div className="credits">
-						<div>
-							Live Weather Data: Automatic Meteorological Station - Park of
-							Aristotle University of Thessaloniki; Department of Meteorology
-							and Climatology, In collaboration with Professor Prodromos Zanis
-						</div>
-						<div>
-							Programming - Software Development: Dimitri Aatos Ellinas
-							<span> </span>
-							Audio Implementation - Sonification: Georgios Mizithras
-						</div>
-					</div>
-				</footer>
-			</Flags>
+				</div>
+			</footer>
 		</div>
 	)
 }
